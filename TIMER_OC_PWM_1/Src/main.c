5c6daec2bc47f9afa0ab918282c546fdf3f6e01d
Hassan ATYQ <hassan.atyq@ext.balyo.com>
2024-12-31 18:24:54 +0100

    Revert "test1"
    This reverts commit 8b88c3f9b25f1f684624709e12ea785ea8897dcc.

--- TIMER_OC_PWM_1/Src/main.c
+++ TIMER_OC_PWM_1/Src/main.c
@@ -61,7 +61,6 @@ int main(void)
   */
 void SystemClock_Config_HSE(uint8_t clock_freq)
 {
-	/* test 1 */
 	RCC_OscInitTypeDef Osc_Init;
 	RCC_ClkInitTypeDef Clock_Init;
     uint8_t flash_latency=0;
